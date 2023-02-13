3b6450add816093fe6e73e5cc5e8fc6fb259b890
JavonPeart <44687372+JavonPeart@users.noreply.github.com>
2023-02-12 19:24:56 -0500

    Completed Lab 2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
+def format_date_joined(date_joined):
+    return "Joined " + date_joined.strftime("%B, %Y") 
 
 ###
 # Routing for your application.
@@ -15,7 +18,15 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Javon Peart")
+
+@app.route('/profile/')
+def profile():
+    """Render the website's profile page."""
+    date_joined = datetime.date(2023, 2, 12)
+    formatted = format_date_joined(date_joined)
+    return render_template('profile.html', date_joined=formatted)
+
 
 
 ###
@@ -45,3 +56,4 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
